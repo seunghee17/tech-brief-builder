8fc547e2644d4a35c746ac2c35c5b831a94107a9
송승희 <victoria9981@fasoo.com>
2026-07-09 16:08:17 +0900

    chore: 미사용 공용 컴포넌트 제거

--- frontend/src/shared/components/Textarea.tsx
+++
@@ -1,5 +0,0 @@
-import type { TextareaHTMLAttributes } from 'react'
-
-export function Textarea(props: TextareaHTMLAttributes<HTMLTextAreaElement>) {
-  return <textarea className="field textarea" {...props} />
-}
